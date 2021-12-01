b79f10027194068cd427f854f6a7d4205d1c7a25
VladUsic <51832198+VladUsic@users.noreply.github.com>
2021-12-01 19:36:13 +0200

    Updates

---
+++ static/js/4.13ec5de5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[4],{112:function(t,e,o){t.exports={title:"HomeView_title__3NhFA",container:"HomeView_container__17jf7"}},120:function(t,e,o){"use strict";o.r(e);o(0),o.p;var n=o(112),i=o.n(n),c=o(1);e.default=function(){return Object(c.jsx)("div",{className:i.a.container,children:Object(c.jsx)("h1",{className:i.a.title,children:"Contacts book app "})})}}}]);
+//# sourceMappingURL=4.13ec5de5.chunk.js.map
